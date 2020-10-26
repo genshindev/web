4f50a83977c464d3feb5a9fb142bb8ce2e457b2b
Marvin Witt <nurmarvin@protonmail.com>
2020-10-26 12:55:46 +0100

    chore: use custom CSS module name interpolation template

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]-[hash:base64:5]",
+  },
+});
